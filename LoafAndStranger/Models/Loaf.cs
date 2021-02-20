82c206e1190906171168ba90906b3f9f8c18a676
Chris LoJacono <clojacono92@gmail.com>
2021-02-20 10:38:15 -0600

    controllers

---
+++ LoafAndStranger/Models/Loaf.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoafAndStranger.Models
+{
+    //Models are for storing pieces of information
+    public class Loaf
+    {
+        public LoafSize Size { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+        public int WeightInOunces  { get; set; }
+        public bool Sliced { get; set; }
+
+
+    }
+    //as long as it isnt used anywhere else this can be in the same file
+    public enum LoafSize
+    {
+        Small,
+        Medium,
+        Large,
+    }
+}
