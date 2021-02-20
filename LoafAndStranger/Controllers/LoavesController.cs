2e6f409a84f330daa023b4e5d30e557799889029
Chris LoJacono <clojacono92@gmail.com>
2021-02-20 11:12:06 -0600

    in class start of controllers

--- LoafAndStranger/Controllers/LoavesController.cs
+++ LoafAndStranger/Controllers/LoavesController.cs
@@ -4,12 +4,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LoafAndStranger.Models;
 
 namespace LoafAndStranger.Controllers
 {
-    [Route("api/LoavesController")]
+    [Route("api/Loaves")]
     [ApiController]
     public class LoavesController : ControllerBase
     {
+        [HttpGet]
+        public List<Loaf> GetAllLoaves()
+        {
+            return new List<Loaf>
+            {
+            new Loaf {Price = 5.50, Size = LoafSize.Medium, Sliced = true, Type = "Rye"},
+            new Loaf {Price = 10.50, Size = LoafSize.Small, Sliced = true, Type = "Pumperknickle"}
+            };
+
+        }
     }
 }
