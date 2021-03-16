3c905943c9c47773a1aa31db9131335620a2c0f9
Chris LoJacono <clojacono92@gmail.com>
2021-03-15 20:57:47 -0500

    almost done

--- LoafAndStranger/DataAccess/LoafRepository.cs
+++ LoafAndStranger/DataAccess/LoafRepository.cs
@@ -9,18 +9,43 @@ namespace LoafAndStranger.DataAccess
 {
     public class LoafRepository
     {
-        static List<Loaf> _loaves = new List<Loaf>
+        const string ConnectionString = "Server=localhost;Database=LoafAndStranger;Trusted_Connection=True"
+
+        //static List<Loaf> _loaves = new List<Loaf>
+        //    {
+        //        new Loaf {Id= 1, Price = 5.50, Size = LoafSize.Medium, Sliced = true, Type = "Rye", WeightInOunces=6},
+        //        new Loaf {Id = 2, Price = 10.50, Size = LoafSize.Small, Sliced = true, Type = "Pumperknickle", WeightInOunces=15}
+        //    };
+
+        private Loaf MapLoaf(SqlDataReader)
+        {
+            var id = (int)reader["Id"]; //explicit cast (throws exception)
+            var size = (LoafSize)reader["Size"];
+            var type = reader["Type"] as string; //implicit cast (returns null)
+            var price = (decimal)reader["Price"];
+            var weightInOunces = (int)reader["WeightInOunces"];
+            var sliced = (bool)reader["Sliced"];
+            var createdDate = (DateTime)reader["CreatedDate"];
+
+            var loaf = new Loaf
             {
-                new Loaf {Id= 1, Price = 5.50, Size = LoafSize.Medium, Sliced = true, Type = "Rye", WeightInOunces=6},
-                new Loaf {Id = 2, Price = 10.50, Size = LoafSize.Small, Sliced = true, Type = "Pumperknickle", WeightInOunces=15}
+                Id = id,
+                Size = size,
+                Type = type,
+                Price = price,
+                WeightInOunces = weightInOunces,
+                Sliced = sliced,
             };
+            return loaf;
+
+        }
 
         public List<Loaf> GetAll()
         {
 
             var loaves = new List<Loaf>();
             //create a connection
-            using var connection = new SqlConnection("Server=localhost;Database=LoafAndStranger;Trusted_Connection=True");
+            using var connection = new SqlConnection(ConnectionString);
             //open the connection
             connection.Open();
 
@@ -37,24 +62,8 @@ namespace LoafAndStranger.DataAccess
             // loop over results
             while (reader.Read()) //reader.Read pulls one row at a time from the db
             {
-                var id = (int)reader["Id"]; //explicit cast (throws exception)
-                var size = (LoafSize)reader["Size"];
-                var type = reader["Type"] as string; //implicit cast (returns null)
-                var price = (decimal)reader["Price"];
-                var weightInOunces = (int)reader["WeightInOunces"];
-                var sliced = (bool)reader["Sliced"];
-                var createdDate = (DateTime)reader["CreatedDate"];
 
-                var loaf = new Loaf
-                {
-                    Id = id,
-                    Size = size,
-                    Type = type,
-                    Price = price,
-                    WeightInOunces = weightInOunces,
-                    Sliced = sliced,
-                };
-                loaves.Add(loaf);
+                loaves.Add(MapLoaf(reader));
             }
             
             return loaves;
@@ -72,21 +81,29 @@ namespace LoafAndStranger.DataAccess
 
             var sql = @"select *
                         from Loaves
-                        Where Id = 1";
+                        Where Id = @Id";
 
             //create a connection
-            using var connection = new SqlConnection("Server=localhost;Database=LoafAndStranger;Trusted_Connection=True");
+            using var connection = new SqlConnection(ConnectionString);
             //open the connection
             connection.Open();
 
             //create a command
             var command = connection.CreateCommand();
 
+            command.Parameters.AddWithValue("Id", id);
+
             command.CommandText = sql;
 
             //send the command to sql server or EXECUTE command
             var reader = command.ExecuteReader();
 
+            if (reader.Read())
+            {
+                var loaf = MapLoaf(reader);
+                return loaf;
+            }
+            return null;
             //var loaf = _loaves.FirstOrDefault(bread => bread.Id == id);
             //return loaf;
         }
